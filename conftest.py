89192236ba162c4e61a0d99e5dfef15354df2d97
Roman Trokin(work) <r.trokin@labmedia.su>
2022-09-21 16:40:07 +0300

    https://stepik.org/lesson/237240/step/9?unit=209628

--- conftest.py
+++ conftest.py
@@ -2,11 +2,32 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help='Choose browser: chrome or firefox')
+    parser.addoption('--language', action='store', default='ru',
+                     help='Choose language: for example ru or en')
+
+
 @pytest.fixture(scope='function')
-def chrome_driver():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    chrome_driver = webdriver.Chrome(options=options)
-    chrome_driver.implicitly_wait(10)
-    yield chrome_driver
-    chrome_driver.quit()
+def driver(request):
+    browser_name = request.config.getoption('browser_name')
+    language_code = request.config.getoption('language')
+    driver = None
+    if browser_name == 'chrome':
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': language_code})
+        driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(1)
+    elif browser_name == 'firefox':
+        options = webdriver.FirefoxOptions()
+        options.set_preference('intl.accept_languages', language_code)
+        driver = webdriver.Firefox(options=options)
+        driver.implicitly_wait(1)
+    else:
+        raise pytest.UsageError(
+            f"--browser_name should be chrome or firefox and use {browser_name}")
+    yield driver
+    driver.quit()
