706e0a8ae0fa4d025d293a3b5f3561e52ca91135
Roman Trokin(work) <r.trokin@labmedia.su>
2022-09-21 17:05:23 +0300

    https://stepik.org/lesson/237240/step/9?unit=209628

--- test_items.py
+++ test_items.py
@@ -3,11 +3,9 @@ import pytest
 from selenium.webdriver.common.by import By
 
 
-class TestProdictPage():
+def test_button_add_basket(driver):
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    driver.get(url)
 
-    def test_order_button(self, driver):
-        url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-        driver.get(url)
-        button_add_to_basket = driver.find_element(
-            By.CSS_SELECTOR, 'button.btn-add-to-basket')
-        assert bool(button_add_to_basket), 'Add to cart button not found'
+    assert driver.find_element(
+        By.CSS_SELECTOR, 'button.btn-add-to-basket'), 'Add to cart button not found'
