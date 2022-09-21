21be62650850138531ce3cf68d02827851851005
Trokin Roman <rtrokin@gmail.com>
2022-09-21 21:11:39 +0300

    https://stepik.org/lesson/237240/step/4?unit=209628

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ def test_button_add_basket(driver):
     url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     driver.get(url)
 
-    assert driver.find_element(
+    assert driver.find_elements(
         By.CSS_SELECTOR, 'button.btn-add-to-basket'), 'Add to cart button not found'
